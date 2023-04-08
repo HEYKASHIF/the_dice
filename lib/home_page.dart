853b8c0628498bea042359f4e8bb30f7c1e195a3
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-08 06:02:16 +0530

    8-april commit43

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,8 +10,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("The Dice"),
+        backgroundColor: Colors.pink,
+        centerTitle: true,
+      ),
     );
   }
 }
